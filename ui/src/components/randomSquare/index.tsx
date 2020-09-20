32d527b5b3293eaec62303665e0457873886908b
ashabbir <abdullah.shabbir@agilent.com>
2020-09-20 21:06:24 +1000

    Calling REST API endpoints using fetch api in Reactfor Factorial, Fibonacci and Random Square

--- ui/src/components/randomSquare/index.tsx
+++ ui/src/components/randomSquare/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from '../shared/layout';
 
 export const RandomSquare = () => {
+    const [square, setSquare] = useState("");
+
+    async function getSquare() {
+        const result = await fetch('api/math/random-square/4');
+        result.json().then(result => setSquare(result));
+    }
+
+    useEffect(() => {
+        getSquare();
+    }, []);
+
     return (
         <Layout pageId="randomSquare">
-            <p>
-                TODO: Calculate random square (i.e. rand(0, n)^2), where random number is between 0 and n.
-            </p>
             <p>
                 User inputs a number (n) in a textbox, then clicks a button to calculate Fib(n) via a corresponding API call.
                 Then the correct result is displayed on the page.
             </p>
+            <form>
+                <input type="text" placeholder="Enter Number" />
+                <br />
+                <button onClick={() => getSquare()}>Submit</button>
+            </form>
+
+            <p>The Square is {JSON.stringify(square)}</p>
         </Layout>
     );
 };
