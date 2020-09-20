351076546e3dea73f4609fef5df9a3976d117a93
ashabbir <abdullah.shabbir@agilent.com>
2020-09-20 21:06:24 +1000

    Calling REST API endpoints using fetch api in Reactfor Factorial, Fibonacci and Random Square

--- ui/src/components/factorial/index.tsx
+++ ui/src/components/factorial/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from '../shared/layout';
 
+
 export const Factorial = () => {
+    const [fact, setFact] = useState("");
+
+    async function getFactorial() {
+        const result = await fetch('api/math/factorial/6');
+        result.json().then(result => setFact(result));
+    }
+
+    useEffect(() => {
+        getFactorial();
+    }, []);
+
     return (
         <Layout pageId="factorial">
-            <p>
-                TODO: Calculate n!
-            </p>
             <p>
                 User inputs a number (n) in a textbox, then clicks a button to calculate n! via a corresponding API call.
                 Then the correct result is displayed on the page.
             </p>
+            <form>
+                <input type="text" placeholder="Enter Number" />
+                <br />
+                <button onClick={() => getFactorial()}>Submit</button>
+            </form>
+
+            <p>The factorial is {JSON.stringify(fact)}</p>
         </Layout>
     );
 };
--- ui/src/components/fibonacci/index.tsx
+++ ui/src/components/fibonacci/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from '../shared/layout';
 
 export const Fibonacci = () => {
+    const [fib, setFib] = useState("");
+
+    async function getFibonacci() {
+        const result = await fetch('api/math/fibonacci/6');
+        result.json().then(result => setFib(result));
+    }
+
+    useEffect(() => {
+        getFibonacci();
+    }, []);
+
     return (
         <Layout pageId="fibonacci">
-            <p>
-                TODO: Calculate n-th Fibonacci number - Fib(n)
-            </p>
             <p>
                 User inputs a number (n) in a textbox, then clicks a button to calculate Fib(n) via a corresponding API call.
                 Then the correct result is displayed on the page.
             </p>
+            <form>
+                <input type="text" placeholder="Enter Number" />
+                <br />
+                <button onClick={() => getFibonacci()}>Submit</button>
+            </form>
+
+            <p>The fibonacci is {JSON.stringify(fib)}</p>
         </Layout>
     );
 };
